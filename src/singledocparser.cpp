b975833ee6c71159dd2189b772feb16e4222818a
vientran <vtran@simquest.com>
2013-10-02 15:35:18 -0400

    Allow setting the emmitter style for nodes
    Add a SetStyle for nodes that can be FlowStyle or BlockStyle. This
    allows you to set the particular style for a node, and is usefull when
    emmitting vectors.
    
    For example, when emmitting a vector the  default BlockStyle is :
    
    - 1.0
    - 1.0
    - 1.0
    
    With FlowStyle, this is put on one line:
    
    [1.0, 1.0, 1.0]

--- src/singledocparser.cpp
+++ src/singledocparser.cpp
@@ -57,7 +57,8 @@ namespace YAML
 		
 		// special case: a value node by itself must be a map, with no header
 		if(m_scanner.peek().type == Token::VALUE) {
-			eventHandler.OnMapStart(mark, "?", NullAnchor);
+			const YAML::EMITTER_STYLE style = DefaultStyle;
+			eventHandler.OnMapStart(mark, "?", NullAnchor, style);
 			HandleMap(eventHandler);
 			eventHandler.OnMapEnd();
 			return;
@@ -94,21 +95,29 @@ namespace YAML
 				m_scanner.pop();
 				return;
 			case Token::FLOW_SEQ_START:
+				eventHandler.OnSequenceStart(mark, tag, anchor, YAML::FlowStyle);
+				HandleSequence(eventHandler);
+				eventHandler.OnSequenceEnd();
+				return;
 			case Token::BLOCK_SEQ_START:
-				eventHandler.OnSequenceStart(mark, tag, anchor);
+				eventHandler.OnSequenceStart(mark, tag, anchor, YAML::BlockStyle);
 				HandleSequence(eventHandler);
 				eventHandler.OnSequenceEnd();
 				return;
 			case Token::FLOW_MAP_START:
+				eventHandler.OnMapStart(mark, tag, anchor, YAML::FlowStyle);
+				HandleMap(eventHandler);
+				eventHandler.OnMapEnd();
+				return;
 			case Token::BLOCK_MAP_START:
-				eventHandler.OnMapStart(mark, tag, anchor);
+				eventHandler.OnMapStart(mark, tag, anchor, YAML::BlockStyle);
 				HandleMap(eventHandler);
 				eventHandler.OnMapEnd();
 				return;
 			case Token::KEY:
 				// compact maps can only go in a flow sequence
 				if(m_pCollectionStack->GetCurCollectionType() == CollectionType::FlowSeq) {
-					eventHandler.OnMapStart(mark, tag, anchor);
+					eventHandler.OnMapStart(mark, tag, anchor, YAML::FlowStyle);
 					HandleMap(eventHandler);
 					eventHandler.OnMapEnd();
 					return;
